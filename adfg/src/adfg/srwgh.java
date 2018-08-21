79b265878a80bd254aeac2718417b0fcb0a9f6cd
LinhsonBui <33462534+LinhsonBui@users.noreply.github.com>
2018-08-21 14:57:00 -0700

    cahnges

---
+++ adfg/src/adfg/srwgh.java
@@ -0,0 +1,15 @@
+package adfg;
+
+public class srwgh {
+
+	public static void main(String[] args) {
+		int x = 0;
+		
+		changeX(x);
+	}
+	
+	public static int changeX(int c) {
+		return c = 10;
+	}
+
+}
---
+++ testProg/FindC/src/garB.java
@@ -0,0 +1,12 @@
+
+public class garB {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		
+
+
+	}
+
+}
