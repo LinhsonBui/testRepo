7567831b026641ba6bde6bd567ccfeda7bfb5e59
dkits <derickL12@yahoo.com>
2018-08-21 15:16:57 -0700

    revert

--- adfg/src/adfg/srwgh.java
+++ adfg/src/adfg/srwgh.java
@@ -10,9 +10,12 @@ public class srwgh {
 	
 	public static int changeY(int c) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 		return c = 9;
 	public static int changeY(int z) {
 		return z = 10;
+=======
+>>>>>>> parent of 4af08a4... return 9
 =======
 >>>>>>> parent of 4af08a4... return 9
 	}
