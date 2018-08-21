9e80e146a8cff21b4ca2dde423980936a6711489
LinhsonBui <33462534+LinhsonBui@users.noreply.github.com>
2018-08-21 15:26:37 -0700

    garbage gone

--- adfg/src/adfg/srwgh.java
+++ adfg/src/adfg/srwgh.java
@@ -5,28 +5,14 @@ public class srwgh {
 	public static void main(String[] args) {
 		int x = 0;
 		
-		changeY(x);
 		changeX(x);
 	}
 	
-<<<<<<< HEAD
-	public static int changeY(int c) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-		return c = 9;
-	public static int changeY(int z) {
-		return z = 10;
-=======
->>>>>>> parent of 4af08a4... return 9
-=======
->>>>>>> parent of 4af08a4... return 9
-=======
-=======
+
 	public static int changeX(int c) {
->>>>>>> parent of 5d13af0... changeX to changeY
+
 		return c = 10;
->>>>>>> parent of d191496... changed variable names
+
 	}
 
 }
